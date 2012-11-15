0856b5be64f592e9fa58e1f353f0b0261eaa12b0
alexandr <alexandr.bedulin@gmail.com>
2012-11-15 10:19:19 +0300

    added some pics

---
+++ gen/com/bedulin/android/app/sp/R.java
@@ -0,0 +1,5 @@
+package com.bedulin.android.app.sp;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
---
+++ src/com/bedulin/android/app/sp/MyActivity.java
@@ -0,0 +1,12 @@
+package com.bedulin.android.app.sp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
